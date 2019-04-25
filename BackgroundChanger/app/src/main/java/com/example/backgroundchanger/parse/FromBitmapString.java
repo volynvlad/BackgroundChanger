6852278e43fa923612691e06001fa3d31e137647
vlad <volynvlad@gmail.com>
2019-04-25 10:58:11 +0300

    deleted:    BackgroundChanger/app/src/main/java/com/example/backgroundchanger/PictureFragment.java 	deleted:    BackgroundChanger/app/src/main/java/com/example/backgroundchanger/PictureListActivity.java 	deleted:    BackgroundChanger/app/src/main/java/com/example/backgroundchanger/PictureListFragment.java 	deleted:    BackgroundChanger/app/src/main/java/com/example/backgroundchanger/SingleFragmentActivity.java 	deleted:    BackgroundChanger/app/src/main/java/com/example/backgroundchanger/entity/Picture.java 	deleted:    BackgroundChanger/app/src/main/java/com/example/backgroundchanger/lab/PictureLab.java 	modified:   BackgroundChanger/app/src/main/java/com/example/backgroundchanger/parse/FromBitmapString.java 	modified:   BackgroundChanger/app/src/main/java/com/example/backgroundchanger/server/Server.java 	deleted:    BackgroundChanger/app/src/main/res/layout/activity_crime_list.xml 	deleted:    BackgroundChanger/app/src/main/res/layout/activity_fragment.xml 	deleted:    BackgroundChanger/app/src/main/res/layout/fragment_picture.xml 	deleted:    BackgroundChanger/app/src/main/res/layout/fragment_picture_list.xml 	deleted:    BackgroundChanger/app/src/main/res/layout/list_item_picture.xml 	.idea/ 	BackgroundChanger/.gitignore 	BackgroundChanger/.idea/encodings.xml 	BackgroundChanger/.idea/misc.xml 	BackgroundChanger/.idea/runConfigurations.xml 	BackgroundChanger/app/.gitignore 	BackgroundChanger/app/build.gradle 	BackgroundChanger/app/proguard-rules.pro 	BackgroundChanger/app/src/androidTest/ 	BackgroundChanger/app/src/main/AndroidManifest.xml 	BackgroundChanger/app/src/main/java/com/example/backgroundchanger/activity/ 	BackgroundChanger/app/src/main/java/com/example/backgroundchanger/add/ 	BackgroundChanger/app/src/main/java/com/example/backgroundchanger/create/ 	BackgroundChanger/app/src/main/java/com/example/backgroundchanger/modify/ 	BackgroundChanger/app/src/main/java/com/example/backgroundchanger/parse/FromFileBitmap.java 	BackgroundChanger/app/src/main/java/com/example/backgroundchanger/parse/FromUriRealPath.java 	BackgroundChanger/app/src/main/java/com/example/backgroundchanger/send/ 	BackgroundChanger/app/src/main/res/drawable-v24/ 	BackgroundChanger/app/src/main/res/drawable/ic_launcher_background.xml 	BackgroundChanger/app/src/main/res/drawable/next.png 	BackgroundChanger/app/src/main/res/drawable/reflection_horizontal.png 	BackgroundChanger/app/src/main/res/drawable/reflection_vertical.png 	BackgroundChanger/app/src/main/res/drawable/rotate_left.png 	BackgroundChanger/app/src/main/res/drawable/rotate_right.png 	BackgroundChanger/app/src/main/res/layout/ 	BackgroundChanger/app/src/main/res/mipmap-anydpi-v26/ 	BackgroundChanger/app/src/main/res/mipmap-hdpi/ 	BackgroundChanger/app/src/main/res/mipmap-mdpi/ 	BackgroundChanger/app/src/main/res/mipmap-xhdpi/ 	BackgroundChanger/app/src/main/res/mipmap-xxhdpi/ 	BackgroundChanger/app/src/main/res/mipmap-xxxhdpi/ 	BackgroundChanger/app/src/main/res/values/colors.xml 	BackgroundChanger/app/src/main/res/values/strings.xml 	BackgroundChanger/app/src/main/res/values/styles.xml 	BackgroundChanger/app/src/main/res/xml/file_paths.xml 	BackgroundChanger/app/src/test/ 	BackgroundChanger/build.gradle 	BackgroundChanger/gradle.properties 	BackgroundChanger/gradle/ 	BackgroundChanger/gradlew 	BackgroundChanger/gradlew.bat 	BackgroundChanger/settings.gradle

--- BackgroundChanger/app/src/main/java/com/example/backgroundchanger/parse/FromBitmapString.java
+++ BackgroundChanger/app/src/main/java/com/example/backgroundchanger/parse/FromBitmapString.java
@@ -5,7 +5,7 @@ import android.graphics.BitmapFactory;
 import android.util.Base64;
 
 public class FromBitmapString {
-    private static Bitmap getBitmapFromString(String stringPicture) {
+    public static Bitmap getBitmapFromString(String stringPicture) {
         byte[] decodedString = Base64.decode(stringPicture, Base64.DEFAULT);
         Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
         return decodedByte;
--- BackgroundChanger/app/src/main/java/com/example/backgroundchanger/server/Server.java
+++ BackgroundChanger/app/src/main/java/com/example/backgroundchanger/server/Server.java
@@ -1,49 +1,68 @@
 package com.example.backgroundchanger.server;
 
-
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.backgroundchanger.activity.MainActivity;
+import com.example.backgroundchanger.activity.ChooseBackground;
+import com.example.backgroundchanger.parse.FromBitmapString;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Server {
     private static final String TAG = "Server";
-    public static void getImage(Context context, String url) {
+    public static void getTest(Context context, String url) {
+        Log.d(TAG, "getTest");
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                response -> Toast.makeText(context, "My response", Toast.LENGTH_LONG).show(),
+                error -> {
+                    Log.e(TAG, "Rest Response " + error.toString());
+                    Toast.makeText(context, "Error to connent to " + url, Toast.LENGTH_LONG).show();
+                }
+        );
+        requestQueue.add(request);
+    }
+    public static void getImage(Context context, String url) {
+        Log.d(TAG, "getImage");
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
         StringRequest objectRequest = new StringRequest(
                 Request.Method.GET,
                 url,
-                null,
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e(TAG, "Rest Response " + error.toString());
-                    }
+                response -> {
+                    Toast.makeText(context, "response successfully", Toast.LENGTH_LONG).show();
+                    Bitmap bitmap = FromBitmapString.getBitmapFromString(response);
+                    ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, bStream);
+                    byte[] bytes = bStream.toByteArray();
+                    Intent intent = new Intent(context, ChooseBackground.class);
+                    intent.putExtra("image", bytes);
+                    context.startActivity(intent);
+                },
+                error -> {
+                    Log.e(TAG, "Rest Response " + error.toString());
+                    Toast.makeText(context, "Error to connect to " + url, Toast.LENGTH_LONG).show();
                 }
 
         );
         requestQueue.add(objectRequest);
     }
-    public static void setImage(Context context, String url, String image) {
+
+    public static void sendImage(Context context, String url, String image) {
+        Log.d(TAG, "sendImage");
         ProgressDialog progressDialog = new ProgressDialog(context);
         RequestQueue queue = Volley.newRequestQueue(context);
 
@@ -52,7 +71,7 @@ public class Server {
                 url,
                 response -> {
                     if (response == null) {
-                        Toast.makeText(context, "response is null", Toast.LENGTH_LONG);
+                        Toast.makeText(context, "response is null", Toast.LENGTH_LONG).show();
                     } else {
                         progressDialog.dismiss();
                         if (response.equals("true")) {
@@ -64,12 +83,14 @@ public class Server {
                 },
                 error -> {
                     Log.e(TAG, "Rest Response " + error.toString());
-                    Toast.makeText(context, "Error to connent to " + url, Toast.LENGTH_LONG).show();
+                    Toast.makeText(context, "Error to connect to " + url, Toast.LENGTH_LONG).show();
                 }
         ) {
             @Override
             protected Map<String, String> getParams() {
+                Log.d(TAG, "getParams");
                 Map<String, String> parameters = new HashMap<>();
+                Log.d(TAG, "parameter - " + image);
                 parameters.put("image", image);
                 return parameters;
             }
